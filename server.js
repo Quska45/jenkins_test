808b081c13e816e440e3379ba8950da9e0e5cdfa
Quska45 <qusrhkdwls45@gmail.com>
2020-12-22 17:01:36 +0900

    auto deploy

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const http = require( 'http' );
 http.createServer(( req, res ) => {
   console.log( '서버 생성' );
   console.log( '된거냐 만거냐 ㅡㅡ' );
+  console.log( '젠킨스 별거 없죠?' );
+  res.write( '<h1>젠킨스 별거 없죠?</h1>' );
   res.end( '<h1> hello</h1>' );
 }).listen( 8080 );
